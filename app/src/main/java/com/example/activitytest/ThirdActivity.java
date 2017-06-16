80c0a8c7fc1cf60e73a281ad54d3f037e1a1fb90
踩花大盗 <mjsxjy@163.com>
2017-06-16 11:32:20 +0800

    my first test android app(《第一行代码》练习)

---
+++ app/src/main/java/com/example/activitytest/ThirdActivity.java
@@ -0,0 +1,26 @@
+package com.example.activitytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_layout);
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ThirdActivity.this, "启动<data>活动！",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+}
